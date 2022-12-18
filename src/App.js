1173ba586d1728699549594a54ed7dbd97dba08b
rgonsahn <rgonsahn@yahoo.com>
2022-12-18 00:36:35 -0500

    added contact and resume comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import About from './components/About'
-
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
+
+import Resume from './components/Resume'
 
 function App() {
   return <div>
     <Header />
     <About />
-    
+   
+    <Contact />
+    <Resume />
+    <Footer />
   </div>
 }
 
